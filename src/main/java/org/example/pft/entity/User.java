a60df884888da029c150afe1381d2d540993f0d8
PPhTan165 <meollo74@gmail.com>
2026-08-24 16:42:28 +0700

    feat(login): lock account for 30 minutes after 5 failed attempts

--- src/main/java/org/example/pft/entity/User.java
+++ src/main/java/org/example/pft/entity/User.java
@@ -19,6 +19,8 @@ public class User {
     private String email;
     private String password;
     private String avatar;
+    private int failedLoginAttempts = 0; //  số lần login sai
+    private LocalDateTime lockedUntil; // Thời gian unblock
     private LocalDateTime createdAt = LocalDateTime.now();
 
     @ManyToMany(fetch = FetchType.EAGER)
