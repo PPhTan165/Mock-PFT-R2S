2c7c0e6203a7d02161ccd935bcd030ed329ace74
PPhTan165 <meollo74@gmail.com>
2026-09-17 18:24:46 +0700

    feat(notification): add notification setting DTOs

---
+++ src/main/java/org/example/pft/dto/notification/NotiData.java
@@ -0,0 +1,17 @@
+package org.example.pft.dto.notification;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class NotiData {
+
+    @NotNull(message = "Daily Reminder is required")
+    private Boolean dailyReminder;
+
+    @NotNull(message = "Tip Enabled is required")
+    private Boolean tipsEnabled;
+
+    @NotNull(message = "Budget Alert is required")
+    private Boolean budgetAlert;
+}
