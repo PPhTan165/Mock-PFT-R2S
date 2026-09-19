0ad1fedd6dc0616c927ea8d481a4a0fb6c7528bf
PPhTan165 <meollo74@gmail.com>
2026-09-19 17:27:25 +0700

    feat(user): add profile update endpoint

---
+++ src/main/java/org/example/pft/controller/UserController.java
@@ -0,0 +1,27 @@
+package org.example.pft.controller;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.example.pft.dto.user.ProfileUserUpdateRequest;
+import org.example.pft.dto.user.UserResponse;
+import org.example.pft.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/profile")
+    public ResponseEntity<UserResponse> updateProfile(
+            @RequestBody @Valid ProfileUserUpdateRequest request){
+        return ResponseEntity.ok().body(userService.updateProfile(request));
+    }
+}
