db7fd85705a5ae7ed729e7151c8f5c345c6b77cb
PPhTan165 <meollo74@gmail.com>
2026-09-21 00:07:12 +0700

    fix(2fa): implement regex for DTO verify

--- src/main/java/org/example/pft/dto/twoFactor/VerifyTwoFactorRequest.java
+++ src/main/java/org/example/pft/dto/twoFactor/VerifyTwoFactorRequest.java
@@ -1,6 +1,7 @@
 package org.example.pft.dto.twoFactor;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,5 +10,6 @@ public class VerifyTwoFactorRequest {
     private String challengeId;
 
     @NotBlank(message = "Verification code is required")
+    @Pattern(regexp = "\\d{6}")
     private String code;
 }
